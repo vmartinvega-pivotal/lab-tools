344d631d1a9c4d5cb095f19cb15b8c7b13c95018
vmartinvega-pivotal <vicente.martin.vega@gmail.com>
2024-03-01 09:45:50 +0000

    automatic export for jump cr with version

--- scripts/jumpcr_deploy.sh
+++ scripts/jumpcr_deploy.sh
@@ -5,6 +5,22 @@ SCRIPT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
 source "${SCRIPT_DIR}"/generic.sh
 
+if [ "$#" -ne 1 ]; then
+    echo "Usage: $0 <jumpcr_version>"
+    exit 1
+fi
+
 ansible-playbook --vault-password-file "${SCRIPT_DIR}/../vault_pass.txt" --extra-vars "ansible_sudo_pass=${sudo_pass}" \
     -i "${inventory_full_path_lab}" \
+    --extra-vars "jumpcr_version=$1" \
     dell.daf.jumpcr_deploy.yml 
+
+ansible-playbook --vault-password-file "${SCRIPT_DIR}/../vault_pass.txt" --extra-vars "ansible_sudo_pass=${sudo_pass}" \
+    -i "${inventory_full_path_lab}" \
+    --extra-vars "jumpcr_version=$1" \
+    dell.daf.jumpcr_pull.yml
+
+ansible-playbook --vault-password-file "${SCRIPT_DIR}/../vault_pass.txt" --extra-vars "ansible_sudo_pass=${sudo_pass}" \
+    -i "${inventory_full_path_lab}" \
+    --extra-vars "jumpcr_version=$1" \
+    dell.daf.jumpcr_export.yml
